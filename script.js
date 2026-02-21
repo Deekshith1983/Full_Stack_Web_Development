d0dff34015ea4dccc880526e8bc5047c288de4ef
Deekshith1983 <dikideeku@gmail.com>
2026-02-21 19:35:34 +0530

    21/02 Bootstrap

---
+++ script.js
@@ -0,0 +1,5 @@
+const exploreBtn = document.getElementById("exploreBtn");
+
+exploreBtn.addEventListener("click", () => {
+    alert("This project shows my intermediate-level practice in HTML, CSS, JavaScript, and Bootstrap.");
+});
