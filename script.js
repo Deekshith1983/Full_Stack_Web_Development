156e326765e7fc0e7bf8daa705a88f412fd40e80
Deekshith1983 <dikideeku@gmail.com>
2026-02-23 18:43:19 +0530

    23/02 Vanta Js Intro

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const exploreBtn = document.getElementById("exploreBtn");
-
-exploreBtn.addEventListener("click", () => {
-    alert("This project shows my intermediate-level practice in HTML, CSS, JavaScript, and Bootstrap.");
+VANTA.NET({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0xffffff,
+    backgroundColor: 0x0a0a0a,
+    points: 10.0,
+    maxDistance: 20.0,
+    spacing: 18.0
 });
+
