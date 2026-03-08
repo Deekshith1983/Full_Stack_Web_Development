a9938f45c3c32317c3394e57c4e7628401f50d43
Deekshith1983 <dikideeku@gmail.com>
2026-03-08 12:36:13 +0530

    08/03 Employee form

--- Employee_form/script.js
+++ Employee_form/script.js
@@ -1,12 +1,28 @@
+// VANTA.BIRDS({
+//   el: document.getElementById('vanta-bg'),
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   scale: 1.00,
+//   scaleMobile: 1.00,
+//   backgroundColor: 0x333940,
+//   backgroundAlpha: 0.30
+// })
+
+
 document.getElementById('employee-form').addEventListener('submit', function (e) {
     e.preventDefault();
 
     const employeeData = { // Creating an object to hold employee data from form inputs
-        name:document.getElementById('name').value,
+        employee_id:document.getElementById('Employee_id').value,
+        firstname:document.getElementById('firstname').value,
+        lastname:document.getElementById('lastname').value,
         email:document.getElementById('inputEmail4').value,
-        password:document.getElementById('inputPassword4').value,
         address:document.getElementById('inputAddress').value,
-        address2:document.getElementById('inputAddress2').value,
+        mobile_number:document.getElementById('inputNumber').value,
+        telephone_number:document.getElementById('inputteleNumber').value,
         city:document.getElementById('inputCity').value,
         state:document.getElementById('inputState').value,
         zip:document.getElementById('inputZip').value
@@ -23,6 +39,7 @@ document.getElementById('employee-form').addEventListener('submit', function (e)
             alert(data);
             console.log('Success:', data);
             document.getElementById('demo').innerHTML = "Form Submitted Successfully";
+            document.getElementById('employee-form').reset(); //this line to clears the form after successful submission
         })
         .catch(error => {
             alert('Error: ' + error);
