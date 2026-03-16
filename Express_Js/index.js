fa1e98d733d38566ba2cd0a75a8dfd46297098d0
Deekshith1983 <dikideeku@gmail.com>
2026-03-16 16:47:37 +0530

    Postman Intro

--- Express_Js/index.js
+++ Express_Js/index.js
@@ -1,10 +1,26 @@
 const express=require("express");
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.send("Hello World!");
     res.statusCode=200;
 });
+
+
+app.get("/user",(req,res)=>{
+    res.sendFile(__dirname + '/index.js');
+});
+
+
+app.post("/Contact",(req,res)=>{
+    console.log(req.body);
+    res.send("Your Data is Delivered")
+});
+
+
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
