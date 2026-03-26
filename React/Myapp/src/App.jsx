6c28c36df93e1c31271d6d43fc73b9719c7ba0ac
Deekshith1983 <dikideeku@gmail.com>
2026-03-26 17:16:26 +0530

    Conditional Rendering and React Hooks(useState)

--- React/Myapp/src/App.jsx
+++ React/Myapp/src/App.jsx
@@ -1,37 +1,12 @@
-import { useState } from "react"
+import MyFirstcompo from './My_firstcompo'
 import './App.css'
-import My_firstcompo from './My_firstcompo.jsx'
-import Openform from './Openform.jsx'
-import { StrictMode } from "react"
-function App() {
-  const [showMyCompo, setShowMyCompo] = useState(false);
-  const [showOpenform, setShowOpenform] = useState(false);
 
-  const handleClick = () => {
-    setShowMyCompo(true);
-    setShowOpenform(false);  
-  };
-
-  const handleC = () => {
-    setShowOpenform(true);
-    setShowMyCompo(false);  
-  };
-
+function App(){
   return (
     <>
       <h1>Hello World!</h1>
-
-      <div className="parent">
-        <button className="mybut" onClick={handleClick}>Click me</button>
-        {showMyCompo && <My_firstcompo />}
-      </div>
-      <br /><br />
-      <div className="parent">
-        <button className="mybut" onClick={handleC}>Open the form</button>
-        {showOpenform && <Openform />}
-      </div>
+      <MyFirstcompo  />
     </>
   );
 }
-
 export default App;
--- React/Myapp/src/My_firstcompo.jsx
+++ React/Myapp/src/My_firstcompo.jsx
@@ -1,17 +1,52 @@
-import './App.css'
+import { useState } from "react";
 
-const PI=3.14;
-function mul(a,b){
-    return a*b;
-}
+// function Car(){
+//   return (
+//     <>
+//       <p>This is a car component.</p>
+//     </>
+//   )
+// }
 
-function My_firstcompo() {
+function Vote(){
   return (
     <>
-      <h1 className='hero'>This is my first component</h1>
-      <p>Value of PI is {PI}</p>
-      <p>Value of multiplication is {mul(5,6)}</p>
+      <p>You can Vote!!</p>
     </>
   )
 }
-export default My_firstcompo;
+
+//Props are used to pass data from one component to another. 
+// They are read-only and cannot be modified by the child component. 
+//Props are passed as an object to the child component and can be accessed using the props keyword.
+// function MyFirstcompo(props) {
+
+//   var age= props.age;
+//     return (
+//         <>
+//             <h1>My First Component</h1>
+//             {/* Condtional rendering */}
+//             {/* {age > 18 ? <Vote /> : <p>You are not old enough to vote.</p>} */}
+//             {age>18 && <Vote />}
+//         </>
+//     );
+// }
+
+
+
+function MyFirstcompo() {
+  
+  const [age, setAge] = useState(0);
+    return (
+      <>
+        <h1>My First Component</h1>
+        <p>Age: {age}</p>
+        {age >= 18 ? <Vote /> : <p>You are not old enough to vote.</p>}
+        <button onClick={() => setAge(age + 1)}>Click Me</button>
+      </>
+    )
+
+}
+
+
+export default MyFirstcompo;
